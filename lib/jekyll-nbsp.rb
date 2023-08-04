ca0b2c991e7be19be3a2d7a529ff38a605600502
Jack Webb <hello@jackwebb.me>
2023-08-04 21:02:43 +0100

    Fix directory layout

--- lib/jekyll-nbsp.rb
+++ lib/jekyll-nbsp.rb
@@ -1,10 +1,1 @@
-module Jekyll
-  module NonBreakingSpaceFilter
-    def nbsp(input)
-      input.to_s.gsub(' ', "\u00A0")
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::NonBreakingSpaceFilter)
-
+require "jekyll-nbsp/jekyll-nbsp"
